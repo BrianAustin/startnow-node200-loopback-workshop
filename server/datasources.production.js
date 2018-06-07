6936aa7a157e8ca5827edce283de2188a55c93e6
Brian <baustin27@gmail.com>
2018-06-07 10:02:37 -0700

    deployment

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -1,7 +1,7 @@
-module.exports = {
-  "MongoDB": {
-    "name": "MongoDB",
-    "connector": "mongodb",
-    "url": process.env.MONGODB_URI || process.env.MONGO_URL || process.env.MONGOHQ_URL
-  }
-}
+// module.exports = {
+//   "MongoDB": {
+//     "name": "MongoDB",
+//     "connector": "mongodb",
+//     "url": process.env.MONGODB_URI
+//   }
+// }
